8e99291d512ccddf1d683e42330b14aacd365053
thirdratecyberpunk <blackbul@aston.ac.uk>
2020-01-06 00:15:46 +0000

    Added annotated game implementation (needs rewriting to support different sender/receiver configurations)

--- env.py
+++ env.py
@@ -8,6 +8,7 @@ import skimage
 from skimage import io
 import matplotlib.pyplot as plt
 
+
 # loads an image from a directory and applies a transformation to it
 def load_image(path):
     image = Image.open(path)
@@ -22,6 +23,9 @@ def load_image(path):
     arr = numpy.array(image)
     plt.imshow(arr)
     plt.show()
+
+def get_all_images(path):
+    return [i for i in path if i.endswith('.jpg')]
 
 class Environment:
     def __init__(self, data_dir, img_dirs, num_classes):
@@ -41,6 +45,39 @@ class Environment:
         # class of the target image
         self.target_class = None
 
+    # TODO: tidy this up, feels a little messy
+    # TODO: try replacing this with a data loader class? more PyTorchy
+    def get_images(self):
+        # picks a random class for target/distractor pair
+        im1_class, im2_class = np.random.choice(list(range(self.num_classes)),
+        2, replace=False)
+        # gets directory for images
+        im1_dir, im2_dir = self.img_dirs[im1_class], self.img_dirs[im2_class]
+
+        ## Temp var for sender training, remove later
+        self.target_class = im1_dir
+
+        im1_path, im2_path = os.path.join(self.data_dir, 'images', im1_dir),
+        os.path.join(self.data_dir, 'images', im2_dir)
+
+        # select random image in dirs
+        im1_files, im2_files = os.listdir(im1_path), os.listdir(im2_path)
+        im1_files = get_all_images(im1_files)
+        im2_files = get_all_images(im2_files)
+
+        # selects a random image for each class
+        im1 = np.random.choice(len(im1_files), 1)[0]
+        im2 = np.random.choice(len(im2_files), 1)[0]
+
+        target_file = os.path.join(im1_path, im1_files[im1])
+        distractor_file = os.path.join(im2_path, im2_files[im2])
+
+        # Load selected image
+        self.target = load_image(target_file)
+        self.distractor = load_image(distractor_file)
+
+        return (self.target, self.distractor)
+
     # updates word sent by the sender agent
     def send_word(self, sent_word):
         self.word = sent_word
@@ -54,5 +91,6 @@ class Environment:
         self.target = None
         self.distractor = None
         self.word = None
+        self.target_class = None
 
 load_image("data/images/cat/0.jpg")
---
+++ game.py
@@ -0,0 +1,138 @@
+# class responsible for running a sequence of the game
+import sys
+import os
+import numpy as np
+from agent_tf2 import Agents
+import env
+import random
+import tensorflow as tf
+import sys
+import argparse
+import yaml
+import torchvision.models as models
+from collections import deque, namedtuple
+
+
+def shuffle_image_activations(im_acts):
+    reordering = np.array(range(len(im_acts)))
+    random.shuffle(reordering)
+    target_ind = np.argmin(reordering)
+    shuffled = im_acts[reordering]
+    return (shuffled, target_ind)
+
+def run_game():
+    # dimensionality of the image embedding
+    image_embedding_dim = config['image_embedding_dim']
+    # dimensionality of the word embedding
+    word_embedding_dim = config['word_embedding_dim']
+    # directory of images
+    data_dir = config['data_dir']
+    # list of all possible classes to load in
+    image_dirs = config['image_dirs']
+    # all possible words in the vocabulary of the agent
+    vocab = config['vocab']
+    # model weights to be passed to the agent
+    model_weights = config['model_weights']
+    # rate at which the agent learns
+    learning_rate = config['learning_rate']
+    # number of iterations to run the game for
+    iterations = config['iterations']
+    # size of batch within batch to sample
+    mini_batch_size = config['mini_batch_size']
+    # size of batch
+    batch_size = config['batch_size']
+    # temperature
+    temperature = config['temperature']
+    # whether the model should be loaded from a pretrained model
+    load_model = config['load_model'] == 'True'
+
+    # creates a pair of sender/receiver agents which are used to populate the
+    # game TODO: replace this with a separate sender/receiver class to experiment
+    # with different population numbers
+    agents = Agents(vocab,
+                    image_embedding_dim,
+                    word_embedding_dim,
+                    learning_rate,
+                    temperature,
+                    batch_size)
+
+    # creates a referential game environment
+    # TODO: modify the environment so it can take more classes/distractors
+    environ = env.Environment(data_dir, image_dirs, 2)
+
+    total_rounds = 0
+    wins = 0
+    losses = 0
+
+    # loads the pretrained VGG16 model
+    model = models.vgg16()
+
+    batch = []
+
+    Game = namedtuple("Game", ["im_acts", "target_acts", "distractor_acts",
+    "word_probs", "image_probs", "target", "word", "selection", "reward"])
+
+    total_reward = 0
+
+    for i in range(iterations):
+        print("Round {}/{}".format(i, iterations), end = "\n")
+        # gets a new target/distractor pair from the environment
+        target_image, distractor_image = environ.get_images()
+        # TODO: check if this is needed
+        # reshapes images into expected shape for VGG model
+        target_image = target_image.reshape((1, 224, 224, 3))
+        distractor_image = distractor_image.reshape((1, 224, 224, 3))
+        # sets the target class variable
+        target_class = environ.target_class
+        # vertically stacks numpy array of image
+        td_images = np.vstack([target_image, distractor_image])
+        # gets actual classifications from prediction of vgg model
+        td_acts = model.predict(td_images)
+        # reshapes predictions
+        target_acts = td_acts[0].reshape((1, 1000))
+        distractor_acts = td_acts[1].reshape((1, 1000))
+        # gets the sender's chosen word and the associated probability
+        word_probs, word_selected = agents.get_sender_word_probs(
+        target_acts, distractor_acts)
+        # gets the target image
+        # TODO: check if this can be modified for more than 2 images
+        reordering = np.array([0,1])
+        random.shuffle(reordering)
+        target = np.where(reordering==0)[0]
+        # sets images as predictions
+        img_array = [target_acts, distractor_acts]
+        im1_acts, im2_acts = [img_array[reordering[i]]
+        for i, img in enumerate(img_array)]
+        # gets the receiver's chosen target and associated probability
+        receiver_probs, image_selected = agents.get_receiver_selection(
+        word_selected, im1_acts, im2_acts)
+        # gives a payoff if the target is the same as the selected image
+        reward = 0.0
+        if target == image_selected:
+            reward = 1.0
+        # adds the game just played to the batch
+        batch.append(Game(shuffled_acts, target_acts, distractor_acts,
+        word_probs, receiver_probs, target, word_selected, image_selected,
+        reward))
+
+        #TODO: implement weight updates
+        if (i+1) % mini_batch_size == 0:
+            print('updating the agent weights')
+
+        print(target_class, reward)
+        tot_reward += reward
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--conf', required=True)
+    args = parser.parse_args()
+    conf = args.conf
+
+    with open(conf) as g:
+        config = yaml.load(g)
+
+    run_game(config)
+
+if __name__ == '__main__':
+    main()
