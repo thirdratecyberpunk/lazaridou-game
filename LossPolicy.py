9cdf9a802a185fbffb176c17f723bd4ccc8125f3
thirdratecyberpunk <blackbul@aston.ac.uk>
2020-02-07 17:43:15 +0000

    LossPolicy is attempting to MINIMISE this value, so value returned is negative

--- LossPolicy.py
+++ LossPolicy.py
@@ -8,7 +8,7 @@ class LossPolicy(Module):
         super(LossPolicy,self).__init__()
 
     def forward(self, selection_prob ,reward):
-        # payoff is the expected value
-        result = np.mean(np.multiply(np.transpose(np.log(selection_prob)), reward))
+        # agents are attempting to minimize the negative expected value
+        result = -1 * np.mean(np.multiply(np.transpose(np.log(selection_prob)), reward))
         res_tensor = torch.tensor(result, requires_grad = True)
         return res_tensor
