a6c15ce6bae13d4b536f4465f4bc52bf95bf1598
thirdratecyberpunk <blackbul@aston.ac.uk>
2020-03-06 12:51:51 +0000

    Added CLI parameters

--- george_receiver.py
+++ george_receiver.py
@@ -15,7 +15,6 @@ import torch.optim as optim
 import torchvision
 from IterableRoundsDataset import IterableRoundsDataset
 
-
 from torchvision import transforms, utils
 import torchvision.models as models
 from PIL import Image
@@ -27,15 +26,27 @@ import numpy as np
 from display import plot_figures
 from itertools import repeat
 
+import argparse
+
 from display import plot_figures, display_comm_succ_graph
+
+parser = argparse.ArgumentParser(description="Train agents to converge upon a language via a referential game.")
+parser.add_argument('--seed', type = int, default = 0, help="Value used as the seed for random generators")
+parser.add_argument('--epochs', type= int, default = 100000, help="Number of epochs to run")
+parser.add_argument('--lr', type=float, default = 0.0001, help="Learning rate")
+parser.add_argument('--vocab_size', type=int, default= 2, help="Size of the vocabulary")
+
+args = parser.parse_args()
+seed = args.seed
+epochs = args.epochs
+lr = args.lr
+vocab_size = args.vocab_size
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# dtype = torch.cuda.FloatTensor
-
-torch.manual_seed(0)
-np.random.seed(0)
-random.seed(0)
+torch.manual_seed(seed)
+np.random.seed(seed)
+random.seed(seed)
 
 img_dirs = ["cat-1", "dog-1"]
 data_dir = "data"
@@ -48,7 +59,6 @@ img_embed_dim = 1000
 word_dict_dim = 2
 
 game_embed_dim = 32
-
 
 model = models.vgg16().to(device)
 
@@ -82,7 +92,8 @@ receiver = Receiver().to(device)
 
 loss = nn.CrossEntropyLoss()
 
-optim_SGD = optim.SGD(receiver.parameters(), lr=0.0001, momentum=0.5)
+# TODO: try using Adam as the optimizer rather than stochastic gradient descent
+optim_SGD = optim.SGD(receiver.parameters(), lr=lr, momentum=0.5)
 # optimizer = optim.Adam(receiver.parameters(), lr = 0.001)
 
 total_rounds = 0
@@ -90,7 +101,9 @@ total_successes = 0
 comm_success_rate = []
 num_rounds = 1
 
-for batch in islice(loader, 100000):
+vocabulary = torch.eye(vocab_size)
+
+for batch in islice(loader, epochs):
         # displaying the batch as a diagram
         # target_display = batch[0]["arr"].permute(1, 2, 0)
         # distractor_display = batch[1]["arr"].permute(1, 2, 0)
@@ -103,7 +116,7 @@ for batch in islice(loader, 100000):
         distractor_display = model(batch[1]["arr"][None,:,:,:].to(device))
         
         # chooses the word as a one hot encoding vector
-        w = torch.eye(2)[batch[0]['category']][None,:].to(device)
+        w = vocabulary[batch[0]['category']][None,:].to(device)
 
         # shuffles the targets and distractors so receiver doesn't learn target based on position
         if random.random()<0.5:
@@ -132,13 +145,11 @@ for batch in islice(loader, 100000):
         comm_success_rate.append(total_successes / total_rounds * 100)
         # applies backpropagation of loss
         optim_SGD.zero_grad()
-        # optimizer.zero_grad()
         L = loss(receiver_scores, torch.tensor([t]).to(device))
         print(L)
         print("_______________________________")
         L.backward()
         optim_SGD.step()
-        # optimizer.step()
 
 display_comm_succ_graph({"perfect sender, default receiver":comm_success_rate})
 
